2e50ec3983fe313b1091f7d393e046d03ec7035b
Thomas Bortolato <61499805+thomas370@users.noreply.github.com>
2024-12-02 14:29:36 +0100

    Update data.js
    Ajout de commentaires

--- pages/api/data.js
+++ pages/api/data.js
@@ -1,24 +1,24 @@
 export default async function handler(req, res) {
   try {
-    console.log("Request Body:", req.body);
+    console.log("Request Body:", req.body); 
 
     const { latitudeInput, longitudeInput } = req.body;
-    const latitude = parseFloat(latitudeInput);
+    const latitude = parseFloat(latitudeInput); //conversion de la longitude et la latitude en nombre 
     const longitude = parseFloat(longitudeInput);
 
-    if (isNaN(latitude) || isNaN(longitude)) {
+    if (isNaN(latitude) || isNaN(longitude)) { //verification si la longitude et la lantitude et un nombre 
       return res.status(400).json({ error: 'Invalid input: latitude and longitude must be numbers' });
     }
     //modification de la request api et essai de l'ajout de l'humidité et de la visibilité sans succé
     const weatherApiUrl = `https://api.open-meteo.com/v1/forecast?timezone=auto&current_weather=true&daily=sunrise,sunset&latitude=${latitude}&longitude=${longitude}`;
     console.log("Generated URL:", weatherApiUrl);
 
-    const getWeatherData = await fetch(weatherApiUrl);
-    if (!getWeatherData.ok) { //verification du fetch de l'api
+    const getWeatherData = await fetch(weatherApiUrl); //appelle pour recupérer les données api
+    if (!getWeatherData.ok) { 
       throw new Error(`Weather API request failed with status ${getWeatherData.status}`);
     }
 
-    const data = await getWeatherData.json();
+    const data = await getWeatherData.json(); // Convertit la réponse de l'API en JSON
     console.log("Weather API Response:", data); //affichage de l'api dans la console
 
     res.status(200).json(data);
